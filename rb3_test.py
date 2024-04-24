f8c0bfe4ed2e5c5156201e9ab81fc19734888f7e
Jihun Lee <gkadnflh@gmail.com>
2024-04-24 15:12:01 +0900

    Add ALOHA mjcf naively

--- rb3_test.py
+++ rb3_test.py
@@ -5,7 +5,7 @@ print(suite.__file__)
 # create environment instance
 env = suite.make(
     env_name="Door", # try with other tasks like "Stack" and "Door"
-    robots="RB3",  # try with other robots like "Sawyer" and "Jaco"
+    robots="ALOHA",  # try with other robots like "Sawyer" and "Jaco"
     has_renderer=True,
     has_offscreen_renderer=False,
     use_camera_obs=False,
